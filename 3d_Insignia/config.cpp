1df325499464c07ecf1431aab56aba45cecaf4e4
fire-391 <36144022+fire-391@users.noreply.github.com>
2021-04-12 18:07:41 -0400

    Add files via upload

---
+++ 3d_Insignia/config.cpp
@@ -0,0 +1,47 @@
+class CfgPatches {
+    class 3d_Insignia {
+        units[] = {};
+        weapons[] = {};
+        requiredAddons[] = {};
+        versio  = 3;
+        author[] = {"3d MRB"};
+    };
+};
+
+class CfgUnitInsignia {
+    class MRB_8312_Patch {
+        displayName = "MSOT 8312 Viking";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\8312.paa";
+    };
+    class MRB_JTAC_Patch {
+        displayName = "JTAC";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\JTAC.paa";
+    };
+    class MRB_SARC_Patch {
+        displayName = "S.A.R.C.";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\SARC.paa";
+    };
+    class MRB_8311_Patch {
+        displayName = "MSOT 8311 Legion";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\8311.paa";
+    };
+    class MRB_8321_Patch {
+        displayName = "MSOT 8321 Hades";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\8321.paa";
+    };
+    class MRB_SOAR_Patch {
+        displayName = "160th SOAR";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\SOAR.paa";
+    };
+	class MRB_S2 {
+        displayName = "S-2 Patch";
+        author = "3d MRB";
+        texture = "3d_Insignia\data\S2.paa";
+	};
+};
