9776e5efdfacb30576e82d683379029497b461ae
Zycorge <zycorge@gmail.com>
2024-01-07 17:37:56 -0700

    Update cfgBoats.hpp

--- 3d_Assets/cfgVehicles/cfgBoats.hpp
+++ 3d_Assets/cfgVehicles/cfgBoats.hpp
@@ -1,11 +1,1 @@
-class CfgVehicles {
-	class B_Boat_Transport_01_F;
-    class MRB_Zodiac: B_Boat_Transport_01_F {
-        displayName = "MRB Zodiac";
-		maxSpeed = 55;
-		enginePower = 47;
-		side = 1;
-		crew = "MRB_Element_CSO";
-		faction = "B_3d_Marine_Raider";
-	};
-};
+
