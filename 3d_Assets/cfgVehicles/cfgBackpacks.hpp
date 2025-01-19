a6162478250860c309197c736f465b495bfba49e
fire-391 <36144022+fire-391@users.noreply.github.com>
2025-01-19 09:16:04 -0500

    Update cfgBackpacks.hpp
    Formatting

--- 3d_Assets/cfgVehicles/cfgBackpacks.hpp
+++ 3d_Assets/cfgVehicles/cfgBackpacks.hpp
@@ -1,19 +1,19 @@
 class B_Carryall_cbr;
 class MRB_SPB_Mag: B_Carryall_cbr {
-    scope = 2;
-    scopeArsenal = 2;
-	  author = "3d Mod Team";
-	  displayName = "3d Carryall Mag Speedball";
-	class TransportMagazines {
+  scope = 2;
+  scopeArsenal = 2;
+  author = "3d Mod Team";
+  displayName = "3d Carryall Mag Speedball";
+  class TransportMagazines {
     MAG_XX(rhs_mag_30Rnd_556x45_M855A1_PMAG, 16);
     MAG_XX(rhsusf_100Rnd_556x45_soft_pouch, 6);
-	};
+  };
 };
 class MRB_SPB_Throw: B_Carryall_cbr {
   scope = 2;
   scopeArsenal = 2;
   author = "3d Mod Team";
-	displayName = "3d Carryall Throwable Speedball";
+  displayName = "3d Carryall Throwable Speedball";
   class TransportMagazines {
     MAG_XX(rhs_mag_an_m8hc, 11);
     MAG_XX(rhs_mag_m67, 20);
@@ -25,7 +25,7 @@ class MRB_SPB_Med: B_Carryall_cbr {
   scopeArsenal = 2;
   author = "3d Mod Team";
   displayName = "3d Carryall Medical Speedball";
-	class TransportItems {
+  class TransportItems {
     ITEM_XX(ACE_quikclot, 20);
     ITEM_XX(ACE_packingBandage, 20);
     ITEM_XX(ACE_plasmaIV_500, 6);
@@ -33,7 +33,7 @@ class MRB_SPB_Med: B_Carryall_cbr {
     ITEM_XX(kat_TXA, 5);
     ITEM_XX(ACE_tourniquet, 4);
     ITEM_XX(kat_ketamine, 5);
-	};
+  };
 };
 /*class B_AssaultPack_Base;
 class rhsusf_assault_eagleaiii_ucp: B_AssaultPack_Base
