987818b843c77dedf911194b2fb857bc12e9b7cb
Zycorge <zycorge@gmail.com>
2025-03-26 17:34:53 -0600

    Update cfgAmmo.hpp
    Add DAGRs with ACE Guidance

--- 3d_Assets/cfgAmmo.hpp
+++ 3d_Assets/cfgAmmo.hpp
@@ -2,7 +2,9 @@
 
 class CfgAmmo 
 {
-    class FlareCore;
+    class MissileCore;
+	class MissileBase;
+	class FlareCore;
     class FlareBase: FlareCore {};
     class Flare_82mm_AMOS_White: FlareCore 
 	{
@@ -19,4 +21,28 @@ class CfgAmmo
         intensity = 1250000;
         timeToLive = 40;
     };
+	class DAGR_Ammo_MELB: MissileBase
+	{
+		autoSeekTarget = 1;
+		class ace_missileguidance
+		{
+			enabled = 1;
+			pitchRate = 15;
+			yawRate = 15;
+			canVanillaLock = 0;
+			defaultSeekerType = "SALH";
+			seekerTypes[] = {"SALH"};
+			defaultSeekerLockMode = "LOBL";
+			seekerLockModes[] = { "LOAL", "LOBL" };
+			defaultNavigationType = "Direct";
+			navigationTypes[] = { "Direct" };
+			seekLastTargetPos = 1;
+			seekerAngle = 30;
+			seekerAccuracy = 1;
+			seekerMinRange = 1;
+			seekerMaxRange = 5000;
+			defaultAttackProfile = "hellfire";
+			attackProfiles[] = {"hellfire"};
+		};
+	};
 };
