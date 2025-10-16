ab77abe56c5d06788f334cdf8f92d832a82ca62f
fire-391 <36144022+fire-391@users.noreply.github.com>
2025-10-16 17:06:27 -0400

    changed inheritance of melb rockets

--- 3d_Assets/cfgAmmo.hpp
+++ 3d_Assets/cfgAmmo.hpp
@@ -5,6 +5,8 @@ class CfgAmmo
     class MissileCore;
 	class MissileBase;
 	class Missile_AGM_02_F;
+    class fza_275_m261;
+    class fza_275_m255a1;
 	class FlareCore;
     class FlareBase: FlareCore {};
     class Flare_82mm_AMOS_White: FlareCore 
@@ -21,6 +23,30 @@ class CfgAmmo
 	{
         intensity = 1250000;
         timeToLive = 40;
+    };
+    class M261_Ammo_MELB: fza_275_m261
+    { 
+        submunitionDirectionType    = "SubmunitionModelDirection";
+        submunitionAmmo             = "fza_m73mpsm";
+        submunitionConeType[]       = {"randomcenter",9};
+        submunitionConeAngle        = 2;
+        triggerDistance             = 500;
+        submunitionInitSpeed        = 500;
+        deleteParentWhenTriggered   = 1;
+    };
+    class M255A1_Ammo_MELB: fza_275_m255a1
+    {
+        submunitionDirectionType    ="SubmunitionModelDirection";
+        submunitionAmmo             = "fza_flec_cluster";
+        submunitionConeType[]       = {"randomcenter",100};
+        submunitionConeAngle        = 12;
+        triggerDistance             = 200;
+        submunitionInitSpeed        = 500;
+        deleteParentWhenTriggered   = 1;
+        submunitionInitialOffset[]  = {0,0,2};
+
+        soundHit1[]                 = {"A3\sounds_f\dummysound",2.5118864,1,5};
+        multiSoundHit[]             = {"soundHit1",1};
     };
 	class DAGR_Ammo_MELB: MissileBase
 	{
@@ -71,3 +97,4 @@ class CfgAmmo
 		};
 	};
 };
+
