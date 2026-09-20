dcae2b0d89afb66224a2cf9dfac669bd7ab8ca68
fire-391 <36144022+fire-391@users.noreply.github.com>
2026-09-19 20:06:44 -0400

    Update cfgMagazines.hpp
    Remove Maaws ammo weight change

--- 3d_Assets/cfgMagazines.hpp
+++ 3d_Assets/cfgMagazines.hpp
@@ -1,11 +1,6 @@
 #include "\3d_Assets\script_component.hpp"
 
 class CfgMagazines {
-    // 3d MRB Ammo weight fix MAAW
-    class CA_LauncherMagazine;
-    class rhs_mag_maaws_HE: CA_LauncherMagazine{mass = 34;};
-    class rhs_mag_maaws_HEAT: CA_LauncherMagazine{mass = 44;};
-    class rhs_mag_maaws_HEDP: CA_LauncherMagazine{mass = 36;};
     class rhs_mag_ATAS_2;
     class 3d_mag_ATAS_2: rhs_mag_ATAS_2 {
         hardpoints[] = {"VTX_ST_L","VTX_ST_R","VTX_ST_OUTBOARD"};
